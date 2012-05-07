a545463cbca8845bb677661daa767f22c3e65831
Tom Cowling <tcowling@thoughtworks.com>
2012-05-07 09:13:15 +0100

    added MessageParser tests

--- test/MessageParserTests.php
+++ test/MessageParserTests.php
@@ -1,10 +1,28 @@
 <?php
 
+require_once 'simpletest/autorun.php';
+require_once '../src/rover/RoverLocation.php';
+require_once '../src/rover/RoverInstructions.php';
+
 class MessageParserTests extends UnitTestCase
 {
-    function testMessageParserReturnsEmptyRoverInstructionsObject(){
+    function testMessageParserReturnsCorrectRoverInstructionsObject(){
         $inputMessageToTest = "2 4 N LRMLMRLMLMRLLLM";
+        $expectedRoverLocation = new RoverLocation(2,4,Direction::NORTH);
+        $expectedRoverInstructions = new RoverInstructions($expectedRoverLocation,"LRMLMRLMLMRLLLM");
 
-        $this->assertEqual(true,false);
+        $instructions = MessageParser::parseInstructions($inputMessageToTest);
+
+        $this->assertEqual($instructions,$expectedRoverInstructions);
+    }
+
+    function testMessageParserReturnsDefaultMessageObject(){
+        $inputMessageToTest = "";
+        $expectedRoverLocation = new RoverLocation(0,0,Direction::NORTH);
+        $expectedRoverInstructions = new RoverInstructions($expectedRoverLocation,"");
+
+        $instructions = MessageParser::parseInstructions($inputMessageToTest);
+
+        $this->assertEqual($instructions,$expectedRoverInstructions);
     }
 }
